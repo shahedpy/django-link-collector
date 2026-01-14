efc96e034892a6d4c518579153e1e78c3fa2800f
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2026-01-14 16:20:20 +0600

    model

--- apps/scraper/models.py
+++ apps/scraper/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class ScrapedLink(models.Model):
+    name = models.CharField(max_length=500, null=True, blank=True)
+    address = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name or self.address
